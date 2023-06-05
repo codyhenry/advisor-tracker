ffa2d4d0c28e14d25443a5946eb8d07fe8ac35ce
codyhenry <codyhenry527@gmail.com>
2023-06-05 18:15:59 -0400

    initial commit

---
+++ advisor-tracker/src/main/java/com/cody/advisortracker/AdvisorTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.cody.advisortracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisorTrackerApplication.class, args);
+	}
+
+}
---
+++ advisor-tracker/src/test/java/com/cody/advisortracker/AdvisorTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cody.advisortracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvisorTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
